d8bcdb5ae5c5992c1ff7a11baf161cb5c933fe23
Mike Elsmore <mike@elsmore.me>
2016-02-22 13:44:03 -0800

    Fix the double click event handler issue

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 $(document).ready(function () {
   // Start PouchDB
-  var db = PouchDB('interconnect');
+  var db = PouchDB('interconnect', {
+    size: 40
+  })
+  // .then(function(){
+  //    console.log('database created')
+  // }).catch(function(err){
+  //     console.log('Error created database',err)
+  // });
 
   // Sync to remote Cloudant
   var remoteUrl = 'https://stravedinhesummovelledis:f614d25cf8ff26cbbd9a36bb02ee5739477f6dd1@b710603f-85d8-4d24-8e8c-7cb34a3aa9ce-bluemix.cloudant.com/interconnect-showcase';
@@ -18,6 +25,7 @@ $(document).ready(function () {
     $.each(result.rows, function (index, doc) {
       templateShowcase(doc.doc);
     });
+    clickEvent();
   }).catch(function (err) {
     console.log(err);
   });
@@ -30,27 +38,40 @@ $(document).ready(function () {
     attachments: true,
   }).on('change', function (change) {
     templateShowcase(change.doc);
+    clickEvent();
   }).on('error', function (err) {
     console.log('Changes', err);
   });
 
   var templateShowcase = function (doc) {
     var backgroundImage = '';
-    $.each(doc._attachments, function (index, image) {
-      console.log(index, image);
-      backgroundImage = `
-      background:url(data:${image.content_type};base64,${image.data}) no-repeat center center fixed;
-      background-size:cover;
-      `;
+    if (doc._attachments){
+      $.each(doc._attachments, function (index, image) {
+        backgroundImage = `
+        background:url(data:${image.content_type};base64,${image.data}) no-repeat center center fixed;
+        background-size:cover;
+        `;
+      });
+    }
+
+    var urls = '';
+    if (typeof doc.url === 'string') {
+      urls += `<p><a target=\"_blank\" href=\"${doc.url}\" >
+        ${doc.url}
+      </a></p>`;
+    } else {
+    $.each(doc.url, function (index, url) {
+      urls += `<p><a target=\"_blank\" href=\"${url}\" >
+        ${url}
+      </a></p>`;
     });
+    }
 
     var template = `<div id=\"${doc._id}\" class=\"column\" style=\"${backgroundImage}\">
       <h2>${doc.title}</h2>
       <div class=\"description\" >
         <p>${doc.description}</p>
-        <a target=\"_blank\" href=\"${doc.url}\" >
-          ${doc.url}
-        </a>
+        ${urls}
       </div>
     </div>`;
 
@@ -59,22 +80,27 @@ $(document).ready(function () {
     } else {
       $('#showcase-list').append(template);
     }
+  };
 
+  var clickEvent = function () {
+    $('.column').unbind('click');
     $('.column').on('click', function (e) {
       e.preventDefault();
+      console.log($(this).hasClass('full-width'));
       if ($(this).hasClass('full-width')) {
         $(this).removeClass('full-width');
       } else {
         var recordId = $(this).attr('id');
+        console.log(recordId);
         db.get(recordId, {
           include_docs: true,
         }).then(function (data) {
           $('#' + recordId).addClass('full-width');
-          console.log(data);
         }).catch(function (err) {
           console.log(err);
         });
       }
+      return false;
     });
-  };
+  }
 });
